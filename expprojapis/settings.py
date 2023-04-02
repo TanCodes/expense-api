8fd85b04722726703e11a0568b97f1525498387a
TANMAY BARVI <64544882+TanCodes@users.noreply.github.com>
2023-04-03 00:21:11 +0530

    Update settings.py

--- expprojapis/settings.py
+++ expprojapis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-czi_bg$szk2td4tx(psd9xwwv7h#3+z)u1=nube%3#lx60tp*w"
+SECRET_KEY = "key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
